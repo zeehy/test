338f4ea64fdd4cad878eeb804f27f54f46767b1b
Judas <132865581+zeehy@users.noreply.github.com>
2024-07-19 11:37:06 +0900

    addition function

--- calculator.py
+++ calculator.py
@@ -1,31 +1,30 @@
 def add(a, b):
-    pass
+    return a+b
 
 
 def subtract(a, b):
-	pass
-
+	return a-b
 
 def multiply(a, b):
-    pass
+    return a*b
 
 
 def divide(a, b):
-    pass
+    return a/b
 
 
 def pow(a, b):
-    pass
+    return a**b
 
 
 def abs(a):
-    pass
+    return a if a>=0 else -a 
 
 
 def mod(a, b):
-    pass
+    return a % b
 
 
 if __name__ == "__main__":
     # 간단한 테스트 코드
-    pass
+    print("abs (-5): ", abs(-5))
